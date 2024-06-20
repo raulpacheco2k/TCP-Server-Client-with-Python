f1d1186418849abf73fa3386ae2042a1b6238e9c
raulpacheco2k <eu@raulpacheco.com.br>
2024-06-19 23:40:43 -0300

    Mantém o servidor rodando mesmo após o client desconectar, dessa forma, permitindo a conexão com vários clients em sequência.

--- server.py
+++ server.py
@@ -10,14 +10,15 @@ with socket(AF_INET, SOCK_STREAM) as object_socket:
     object_socket.listen(3)
     print(f'Listening on {server["ip"]}:{server["port"]}')
 
-    print('Awaiting client...')
-    connection, client = object_socket.accept()
-
-    with connection:
-        print(f'Connected with {client[0]}:{client[1]}')
-        while True:
-            message_received = str(bytes(connection.recv(1024)), 'utf-8')
-            print(f'Received: {message_received}')
-            if not message_received:
-                break
-            connection.sendall(b'Read confirmation.')
+    while True:
+        print('Awaiting client...')
+        connection, client = object_socket.accept()
+        with connection:
+            print(f'Connected with {client[0]}:{client[1]}')
+            while True:
+                message_received = str(bytes(connection.recv(1024)), 'utf-8')
+                print(f'{client[0]}:{client[1]}: {message_received}')
+                if not message_received:
+                    print(f"{client[0]}:{client[1]} disconnected")
+                    break
+                connection.sendall(b'Read confirmation.')
